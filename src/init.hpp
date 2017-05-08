cb48faea57babe4bf51f6d77a49bc6185c7d0ed1
lwang-astro <longwang.astro@gmail.com>
2017-05-08 11:44:57 +0900

    Add missed init.hpp

---
+++ src/init.hpp
@@ -0,0 +1,77 @@
+#pragma once
+
+// Obtain Radius parameters
+template<class Tpsys>
+void GetR(PS::F64 &r_in,
+          PS::F64 &r_out,
+          PS::F64 &m_average,
+          PS::F64 &dt,
+          PS::F64 &vel_disp,
+          const Tpsys & system_soft,
+          const PS::F64 ratio_r_cut){
+    const PS::S32 n_loc = system_soft.getNumberOfParticleLocal();
+    PS::F64vec vel_cm_loc = 0.0;
+    PS::F64 mass_cm_loc = 0.0;
+    //    PS::F64 mmax_loc = system_soft[0].mass;
+    //    PS::F64 mmin_loc = mmax_loc;
+    for(PS::S32 i=0; i<n_loc; i++){
+      mass_cm_loc += system_soft[i].mass;
+      vel_cm_loc += system_soft[i].mass * system_soft[i].vel;
+      //      if (system_soft[i].mass>mmax_loc) mmax_loc = system_soft[i].mass;
+      //      if (system_soft[i].mass<mmin_loc) mmin_loc = system_soft[i].mass;
+    }
+    PS::F64    mass_cm_glb = PS::Comm::getSum(mass_cm_loc);
+    PS::F64vec vel_cm_glb  = PS::Comm::getSum(vel_cm_loc);
+    //    PS::F64    mmax = PS::Comm::getMaxValue(mmax_loc);
+    //    PS::F64    mmin = PS::Comm::getMaxValue(mmin_loc);
+      
+    vel_cm_glb /= mass_cm_glb;
+    PS::F64 vel_sq_loc = 0.0;
+    for (PS::S32 i=0; i<n_loc; i++){
+      PS::F64vec dv = system_soft[i].vel - vel_cm_glb;
+      vel_sq_loc += dv * dv;
+    }
+
+    const PS::S32    n_glb      = PS::Comm::getSum(n_loc);
+    const PS::F64    vel_sq_glb = PS::Comm::getSum(vel_sq_loc);
+    vel_disp   = sqrt(vel_sq_glb / 3.0 / (PS::F64)n_glb);
+
+    PS::F64 average_mass_glb = mass_cm_glb/(PS::F64)n_glb;
+    m_average = average_mass_glb;
+
+    if (r_out>0) {
+      r_in = r_out * ratio_r_cut;
+    }
+    else {
+      r_in = 3.0*average_mass_glb / (vel_disp*vel_disp);
+      // remove 2.33 factor
+      // r_out = r_in * pow(mmax/average_mass_glb/gmin,1.0/7.0);
+      r_out = r_in / ratio_r_cut;
+    }
+    //    r_search = r_out*(1.0 + search_factor);
+    PS::F64 dt_origin = 0.125*sqrt(r_out*r_out+r_in*r_in) / vel_disp;
+    dt = 1.0;
+    if (dt_origin<1) while (dt>dt_origin) dt *= 0.5;
+    else {
+      while (dt<=dt_origin) dt *= 2.0;
+      dt *= 0.5;
+    }
+}
+
+template<class Tpsys>
+void SetBinaryRsearch(Tpsys & psys, const PS::S32 n_bin, const PS::F64 g_min, const PS::F64 r_def) {
+  double gamma = std::pow(1.0/g_min,0.33333);
+  for (PS::S32 i=0; i<2*n_bin; i+=2) {
+    if (psys[i].r_search<=0||psys[i+i].r_search<=0) {
+      double a,ecc;
+      PosVel2AxEcc(a,ecc,psys[i].pos, psys[i+1].pos, psys[i].vel, psys[i+1].vel, psys[i].mass, psys[i+1].mass);
+      if(a>0) psys[i+1].r_search = psys[i].r_search = a*(1+ecc)*gamma;
+      else psys[i+1].r_search = psys[i].r_search = r_def;
+    }
+  }
+}
+
+template<class Tpsys>
+void SetSingleRsearch(Tpsys & psys, const PS::S32 n, const PS::S32 n_off, const PS::F64 r_search) {
+  for (PS::S32 i=n_off; i<n; i++) if(psys[i].r_search<=0) psys[i].r_search = r_search;
+}
