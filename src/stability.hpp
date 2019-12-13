0b6f50c58a68c0da1f668c4a679c1ea6f4a687af
Long Wang <longwang.astro@gmail.com>
2019-12-13 10:29:14 +0900

    mv initdata.sh to tools directory; update comments in stablility.hpp

--- src/stability.hpp
+++ src/stability.hpp
@@ -22,14 +22,14 @@ public:
        @param[in] _bin_in: inner binary
        @param[in] _bin_out: outer binary (m1 is the inner c.m. mass)
        @param[in] _incline: inclination angle between inner and outer orbit (radians)
-       @param[in] _dt: time interval for stable check
+       @param[in] _t_crit: time interval for stable check
        @param[in] _is_cm_in_first: true: _bin_out m1 is the inner c.m. mass; otherwise m2
        \return stability factor <1 stable; >1 unstable
     */
     static PS::F64 stable3body(const Bin& _bin_in,
                                const Bin& _bin_out,
                                const PS::F64 _incline,
-                               const PS::F64 _dt,
+                               const PS::F64 _t_crit,
                                const bool is_cm_in_first) {
 
         PS::F64 fac = 1.0 - 2.0*_bin_in.ecc/3.0 * (1.0 - 0.5*_bin_in.ecc*_bin_in.ecc) 
@@ -47,7 +47,7 @@ public:
         PS::F64 g = std::sqrt(std::max(_bin_in.m1,_bin_in.m2) /min)*(1.0 + mout/min);
     
         //Adopt at least 10,000 outer orbits for random walk time-scale.
-        PS::F64 q = 1.52*std::pow(std::sqrt(std::min(_dt/_bin_out.period,10000.0))/(1.0 - _bin_out.ecc),1.0/6.0)*std::pow(fac*g,1.0/3.0);
+        PS::F64 q = 1.52*std::pow(std::sqrt(std::min(_t_crit/_bin_out.period,10000.0))/(1.0 - _bin_out.ecc),1.0/6.0)*std::pow(fac*g,1.0/3.0);
 
         PS::F64 peri_out = _bin_out.semi * (_bin_out.ecc + 1.0);
         PS::F64 rp = peri_out/_bin_in.semi;
@@ -60,14 +60,14 @@ public:
     //! check two-body stability
     /* return       case
        Unstable:    false   hyperbolic orbit
-       stable:      true    apo-center < _rcrit && period < _tcrit 
+       stable:      true    apo-center < _r_crit && period < _t_crit 
                     false   others
         
        @param[in,out] _bin: binary information
-       @param[in] _rcrit: distance criterion
-       @param[in] _tcrit: period criterion
+       @param[in] _r_crit: distance criterion to select stable binary
+       @param[in] _t_crit: period criterion to select stable binary
     */
-    static bool stable2check(const Bin& _bin, const PS::F64 _rcrit, const PS::F64 _tcrit) {
+    static bool stable2check(const Bin& _bin, const PS::F64 _r_crit, const PS::F64 _t_crit) {
 
 #ifdef STABLE_CHECK_DEBUG_PRINT
         std::cerr<<"STAB2: semi="<<_bin.semi
@@ -91,7 +91,7 @@ public:
         // binary case
         PS::F64 apo = _bin.semi*(1.0+_bin.ecc);
 
-        if (apo<_rcrit&&_bin.period<_tcrit)  {
+        if (apo<_r_crit&&_bin.period<_t_crit)  {
 #ifdef STABLE_CHECK_DEBUG_PRINT
             std::cerr<<"STAB2 accept: Stable"<<std::endl;
 #endif 
@@ -99,8 +99,8 @@ public:
         }
         else {
 #ifdef STABLE_CHECK_DEBUG_PRINT
-            std::cerr<<"STAB2 reject: Stable but too large orbit, apo: "<<apo<<" ecc: "<<_bin.ecc<<" r_crit: "<<_rcrit
-                     <<" period: "<<_bin.period<<" tcrit: "<<_tcrit<<std::endl;
+            std::cerr<<"STAB2 reject: Stable but too large orbit, apo: "<<apo<<" ecc: "<<_bin.ecc<<" r_crit: "<<_r_crit
+                     <<" period: "<<_bin.period<<" t_crit: "<<_t_crit<<std::endl;
 #endif 
             return false;
         }
@@ -114,14 +114,14 @@ public:
        true               stab3 <1 
        @param[in] _bin: inner orbit parameter
        @param[in] _bout: outer orbit parameter
-       @param[in] _rcrit: distance criterion
-       @param[in] _tcrit: period criterion
+       @param[in] _r_crit: distance criterion to select stable binary
+       @param[in] _t_crit: period criterion to select stable binary
        @param[in] _is_cm_in_first: true: _bin_out m1 is the inner c.m. mass; otherwise m2
     */
     static bool stable3check(const Bin& _bin,
                              const Bin& _bout,
-                             const PS::F64 _rcrit,
-                             const PS::F64 _tcrit,
+                             const PS::F64 _r_crit,
+                             const PS::F64 _t_crit,
                              const PS::F64 _is_cm_in_first) {
 #ifdef STABLE_CHECK_DEBUG_PRINT
         std::cerr<<"STAB3 bout semi="<<_bout.semi
@@ -150,17 +150,17 @@ public:
         PS::F64 apo_out=_bout.semi*(1.0+_bout.ecc);
 
         // too large orbit
-        if(apo_out>_rcrit) {
+        if(apo_out>_r_crit) {
 #ifdef STABLE_CHECK_DEBUG_PRINT
-            std::cerr<<"STAB3 reject: Too large outer orbit, apo_out: "<<apo_out<<" rcrit: "<<_rcrit<<std::endl;
+            std::cerr<<"STAB3 reject: Too large outer orbit, apo_out: "<<apo_out<<" r_crit: "<<_r_crit<<std::endl;
 #endif
             return false;
         } 
 
         // too large period
-        if(_bout.period>_tcrit) {
+        if(_bout.period>_t_crit) {
 #ifdef STABLE_CHECK_DEBUG_PRINT
-            std::cerr<<"STAB3 reject: Too large outer period, period_out: "<<_bout.period<<" tcrit: "<<_tcrit<<std::endl;
+            std::cerr<<"STAB3 reject: Too large outer period, period_out: "<<_bout.period<<" t_crit: "<<_t_crit<<std::endl;
 #endif
             return false;
         }
@@ -168,7 +168,7 @@ public:
         // stability check
         // inclination between inner and outer orbit
         PS::F64 incline=std::acos(std::min(1.0, _bout.am*_bin.am/std::sqrt((_bout.am*_bout.am)*(_bin.am*_bin.am))));
-        PS::F64 stab3 = stable3body(_bin, _bout, incline, _tcrit, _is_cm_in_first);
+        PS::F64 stab3 = stable3body(_bin, _bout, incline, _t_crit, _is_cm_in_first);
         if(stab3>1) {
             // Unstable case
 #ifdef STABLE_CHECK_DEBUG_PRINT
@@ -188,7 +188,7 @@ public:
     //! Four-body (B-B) stability check
     /*             return     tstep   stable_factor      case
                    Unstable:  false     -1         -1          hyperbolic outer orbit
-                   false     -1         -1          apo-center outer > _rcrit
+                   false     -1         -1          apo-center outer > _r_crit
                    false     -1         -1          period outer > 0.25 * dt_tree
                    true      inner      -stab3_max  stab3_1 >0.8 || stab3_2 > 0.8 & apo_out <= r_out
                    false     -1         -1                                        & apo_out >  r_out
@@ -198,16 +198,14 @@ public:
                    @param[in] _bout: outer orbit parameter
                    @param[in] _bin1: first inner orbit parameter
                    @param[in] _bin2: second inner orbit parameter
-                   @param[in] _rbin: binary radius criterion from input
-                   @param[in] _rin: inner radius of soft-hard changeover function
-                   @param[in] _rout: outer radius of soft-hard changeover function
-                   @param[in] _dt_tree: tree time step
+                   @param[in] _r_crit: distance criterion to select stable binary
+                   @param[in] _t_crit: period criterion to select stable binary
     */
     static bool stable4check(const Bin& _bin1,
                              const Bin& _bin2,
                              const Bin& _bout,
-                             const PS::F64 _rcrit,
-                             const PS::F64 _tcrit) {
+                             const PS::F64 _r_crit,
+                             const PS::F64 _t_crit) {
 #ifdef STABLE_CHECK_DEBUG_PRINT
         std::cerr<<"STAB4 bout semi="<<_bout.semi
                  <<" ecc="<<_bout.ecc
@@ -247,17 +245,17 @@ public:
 
     
         // too large orbit
-        if(apo_out>_rcrit) {
+        if(apo_out>_r_crit) {
 #ifdef STABLE_CHECK_DEBUG_PRINT
-            std::cerr<<"STAB4 reject: Unstable, Too large outer orbit, apo_out: "<<apo_out<<" rcrit: "<<_rcrit<<std::endl;
+            std::cerr<<"STAB4 reject: Unstable, Too large outer orbit, apo_out: "<<apo_out<<" r_crit: "<<_r_crit<<std::endl;
 #endif
             return false;
         } 
 
         // too large period
-        if(_bout.period>_tcrit) {
+        if(_bout.period>_t_crit) {
 #ifdef STABLE_CHECK_DEBUG_PRINT
-            std::cerr<<"STAB4 reject: Too large outer period, period_out: "<<_bout.period<<" tcrit: "<<_tcrit<<std::endl;
+            std::cerr<<"STAB4 reject: Too large outer period, period_out: "<<_bout.period<<" t_crit: "<<_t_crit<<std::endl;
 #endif
             return false;
         }
@@ -266,8 +264,8 @@ public:
         // inclination between inner and outer orbit
         PS::F64 incl1=std::acos(std::min(1.0, _bout.am*_bin1.am/std::sqrt((_bout.am*_bout.am)*(_bin1.am*_bin1.am))));
         PS::F64 incl2=std::acos(std::min(1.0, _bout.am*_bin2.am/std::sqrt((_bout.am*_bout.am)*(_bin2.am*_bin2.am))));
-        PS::F64 stab3_1 = stable3body(_bin1, _bout, incl1, _tcrit, true);
-        PS::F64 stab3_2 = stable3body(_bin2, _bout, incl2, _tcrit, false);
+        PS::F64 stab3_1 = stable3body(_bin1, _bout, incl1, _t_crit, true);
+        PS::F64 stab3_2 = stable3body(_bin2, _bout, incl2, _t_crit, false);
         if(stab3_1>0.8||stab3_2>0.8) {
             // Unstable case
 #ifdef STABLE_CHECK_DEBUG_PRINT
