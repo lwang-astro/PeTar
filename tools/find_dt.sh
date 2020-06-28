b437958093b0e82ec0dde4d98992a1502ea9b051
Long Wang <longwang.astro@live.com>
2020-06-28 13:41:30 +0900

    Update find_dt.sh to show correct step size and number counter when step is not a regularized value (0.5^{int})

--- tools/find_dt.sh
+++ tools/find_dt.sh
@@ -75,9 +75,10 @@ do
     tend=`echo $dt |awk -v tzero=$tzero '{print tzero+$1*6.01}' `
     ${prefix} $pbin -w 0 -t $tend $opts  -s $dt -o $dt $fname &>.check.perf.$dt.log
     egrep 'Wallclock' -A 3 .check.perf.$dt.log |sed -n '/^\ *[0-9]/ p'|awk '{if (NR>1 && NR%2==0) print $1,$2+$3+$4+$8,$7+$9,$6+$10+$11,$12+$13}' >.check.perf.$dt.tperf
-    tperf=(`awk -v dt=$dt 'BEGIN{t=1e10; ns=1.0/dt; th=0; ts=0; tc=0; td=0;} {if (t>$1) {t=$1; th=$2; ts=$3; tc=$4; td=$5;}} END{print t*ns,th*ns,ts*ns,tc*ns,td*ns}' .check.perf.$dt.tperf`)
+    dt_reg=`egrep dt_soft .check.perf.$dt.log|awk '{OFMT="%.14g"; print $3}'`
+    tperf=(`awk -v dt=$dt_reg 'BEGIN{t=1e10; ns=1.0/dt; th=0; ts=0; tc=0; td=0;} {if (t>$1) {t=$1; th=$2; ts=$3; tc=$4; td=$5;}} END{print t*ns,th*ns,ts*ns,tc*ns,td*ns,ns}' .check.perf.$dt.tperf`)
     de=`grep 'dE(SD)' .check.perf.$dt.log|awk '{print $11}'`
-    echo 'check tree step: '$dt', wallclock time for one time unit: '${tperf[0]}'  hard: '${tperf[1]}'  soft: '${tperf[2]}'   clustering: '${tperf[3]}'   domain: '${tperf[4]}
+    echo 'check tree step: '$dt_reg', wallclock time for one time unit: '${tperf[0]}'  hard: '${tperf[1]}'  soft: '${tperf[2]}'   clustering: '${tperf[3]}'   domain: '${tperf[4]}'   step number: '${tperf[5]}
     echo '  wallclock time first 6 steps: '`awk '{print $1}' .check.perf.$dt.tperf`
     echo '  cumulative error: '$de
     check_flag=`echo $tperf |awk -v pre=$tperf_pre '{if ($1<pre) print "true"; else print "false"}'`
