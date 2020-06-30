2c79a714925fe083395d40f0130c22815a95f0bd
Long Wang <longwang.astro@live.com>
2020-06-30 10:38:00 +0900

    Update find_dt to support general petar options and force the termination of test if no response for long time

--- tools/find_dt.sh
+++ tools/find_dt.sh
@@ -8,6 +8,7 @@ unset nmpi
 unset nomp
 unset fname
 unset prefix_flag
+unset opts
 unset unit
 unset G
 
@@ -18,23 +19,27 @@ do
 	    echo 'Check the tree soft step for best performance, ';
 	    echo 'Usage: petar.init [options] [data file name]';
 	    echo 'Options:';
-	    echo '  -r: user defined prefix before program name, if used -m and -o are suppressed (default: )'
+	    echo '  -r: user defined prefix before program name, please use " " to enclose the options.'
+            echo '      If used, -m and -o are suppressed (default: "")'
+	    echo '  -a: user defined options used for petar commander, please use " " to enclose the options.'
+	    echo '      For example, "-b [binary number], -u [unit set] -G [gravitaitonal constant]" (default: "")'
 	    echo '  -p: petar commander name (default: petar)';
 	    echo '  -s: base tree step size (default: auto)';
-	    echo '  -b: binary number (default: 0)';
+#	    echo '  -b: binary number (default: 0)';
 	    echo '  -m: number of MPI processors (default: 1)';
 	    echo '  -o: number of OpenMP processors (default: auto)';
-	    echo '  -u: petar unit set (default: 0)';
-	    echo '  -G: gravitational constant (default: 1)'
+#	    echo '  -u: petar unit set (default: 0)';
+#	    echo '  -G: gravitational constant (default: 1)'
 	    exit;;
 	-r) shift; run=$1; prefix_flag=yes; shift;;
+	-a) shift; opts=$1; shift;;
 	-p) shift; pbin=$1; shift;;
 	-s) shift; dt_base=$1; shift;;
-	-b) shift; bnum=$1; shift;;
+#	-b) shift; bnum=$1; shift;;
 	-m) shift; nmpi=$1; shift;;
 	-o) shift; nomp=$1; shift;;
-	-u) shift; unit=$1; shift;;
-	-G) shift; G=$1; shift;;
+#	-u) shift; unit=$1; shift;;
+#	-G) shift; G=$1; shift;;
 	*) fname=$1;shift;;
     esac
 done
@@ -44,14 +49,14 @@ if [ ! -e $fname ] | [ -z $fname ] ; then
     exit
 fi
 
-opts=''
+#[ -z $opts ] && opts=''
 [ -z $pbin ] && pbin=petar
 [ -z $nomp ] || prefix='env OMP_NUM_THREADS='$nomp
 [ -z $nmpi ] || prefix=$prefix' mpiexec -n '$nmpi
-[ -z $bnum ] || opts=$opts' -b '$bnum
+#[ -z $bnum ] || opts=$opts' -b '$bnum
 [ -z $prefix_flag ] || prefix=$run
-[ -z $unit ] || opts=$opts' -u '$unit
-[ -z $G ] || opts=$opts' -G '$G
+#[ -z $unit ] || opts=$opts' -u '$unit
+#[ -z $G ] || opts=$opts' -G '$G
 
 echo 'commander: '$prefix' '$pbin' '$opts
 
@@ -69,21 +74,27 @@ tzero=`head -1 $fname|awk '{print $3}'`
 dt=$dt_base
 dt_min=$dt
 check_flag=true
+tcum=10000 # sec
 while [[ $check_flag == true ]]
 do
     dt=`echo $dt|awk '{OFMT="%.14g"; print $1*2.0}'`
     tend=`echo $dt |awk -v tzero=$tzero '{print tzero+$1*6.01}' `
-    ${prefix} $pbin -w 0 -t $tend $opts  -s $dt -o $dt $fname &>.check.perf.$dt.log
+    ${prefix} timeout $tcum $pbin -w 0 -t $tend $opts  -s $dt -o $dt $fname &>.check.perf.$dt.log
     egrep 'Wallclock' -A 3 .check.perf.$dt.log |sed -n '/^\ *[0-9]/ p'|awk '{if (NR>1 && NR%2==0) print $1,$2+$3+$4+$8,$7+$9,$6+$10+$11,$12+$13}' >.check.perf.$dt.tperf
-    dt_reg=`egrep dt_soft .check.perf.$dt.log|awk '{OFMT="%.14g"; print $3}'`
-    tperf=(`awk -v dt=$dt_reg 'BEGIN{t=1e10; ns=1.0/dt; th=0; ts=0; tc=0; td=0;} {if (t>$1) {t=$1; th=$2; ts=$3; tc=$4; td=$5;}} END{print t*ns,th*ns,ts*ns,tc*ns,td*ns,ns}' .check.perf.$dt.tperf`)
-    de=`grep 'dE(SD)' .check.perf.$dt.log|awk '{print $11}'`
-    echo 'check tree step: '$dt_reg', wallclock time for one time unit: '${tperf[0]}'  hard: '${tperf[1]}'  soft: '${tperf[2]}'   clustering: '${tperf[3]}'   domain: '${tperf[4]}'   step number: '${tperf[5]}
-    echo '  wallclock time first 6 steps: '`awk '{print $1}' .check.perf.$dt.tperf`
-    echo '  cumulative error: '$de
-    check_flag=`echo $tperf |awk -v pre=$tperf_pre '{if ($1<pre) print "true"; else print "false"}'`
-    [[ $check_flag == true ]] && dt_min=$dt
-    tperf_pre=$tperf
+    if [[ `wc -l .check.perf.$dt.tperf|awk '{print $1}'` -ge 6 ]]; then 
+	dt_reg=`egrep dt_soft .check.perf.$dt.log|awk '{OFMT="%.14g"; print $3}'`
+	tperf=(`awk -v dt=$dt_reg 'BEGIN{t=1e10; ns=1.0/dt; th=0; ts=0; tc=0; td=0;} {if (t>$1) {t=$1; th=$2; ts=$3; tc=$4; td=$5;}} END{print t*ns,th*ns,ts*ns,tc*ns,td*ns,ns}' .check.perf.$dt.tperf`)
+	de=`grep 'dE(SD)' .check.perf.$dt.log|awk '{print $11}'`
+	echo 'check tree step: '$dt_reg', wallclock time for one time unit: '${tperf[0]}'  hard: '${tperf[1]}'  soft: '${tperf[2]}'   clustering: '${tperf[3]}'   domain: '${tperf[4]}'   step number: '${tperf[5]}
+	tcum=`awk 'BEGIN{t=0} {t=t+$1} END{if (t<1) {t=1}; print t*3}' .check.perf.$dt.tperf`
+	echo '  wallclock time first 6 steps: '`awk '{print $1}' .check.perf.$dt.tperf`' sum: '$tcum
+	echo '  cumulative error: '$de
+	check_flag=`echo $tperf |awk -v pre=$tperf_pre '{if ($1<pre) print "true"; else print "false"}'`
+	[[ $check_flag == true ]] && dt_min=$dt
+	tperf_pre=$tperf
+    else
+	check_flag=false
+    fi
     #rm -f .check.perf.$dt.log
     #rm -f .check.perf.$dt.tperf
 done
