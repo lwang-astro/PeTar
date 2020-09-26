a0b94f14789e4c4ab66d7ff617889e7311c423e8
Long Wang <longwang.astro@live.com>
2020-09-26 11:00:31 +0900

    Update the f32 correction in 4th order gradient correction function

--- src/hard.hpp
+++ src/hard.hpp
@@ -1541,14 +1541,19 @@ private:
         const PS::F32 r_out2 = r_out_32 * r_out_32;
         PS::F32vec ri_32 = PS::F32vec(_pi.pos.x, _pi.pos.y, _pi.pos.z);
         PS::F32vec rj_32 = PS::F32vec(_pj.pos.x, _pj.pos.y, _pj.pos.z);
+        PS::F32vec ai_32 = PS::F32vec(_pi.acc.x, _pi.acc.y, _pi.acc.z);
+        PS::F32vec aj_32 = PS::F32vec(_pj.acc.x, _pj.acc.y, _pj.acc.z);
         PS::F32vec dr_32 = ri_32 - rj_32;
+        PS::F32vec da_32 = ai_32 - aj_32;
         PS::F32 dr2_eps_32 = dr_32*dr_32 + (PS::F32)eps_sq;
+        const PS::F32 drda_32 = dr_32*da_32;
         const PS::F32 dr2_max = (dr2_eps_32 > r_out2) ? dr2_eps_32 : r_out2;
         const PS::F32 drinv_max = 1.0/sqrt(dr2_max);
         const PS::F32 gmor_max = G*_pj.mass * drinv_max;
         const PS::F32 drinv2_max = drinv_max*drinv_max;
         const PS::F32 gmor3_max = gmor_max * drinv2_max;
-        const PS::F32 alpha_max = drda * drinv2_max;
+        const PS::F32 alpha_max = drda_32 * drinv2_max;
+        const PS::F32vec acorr_max = gmor3_max * (da_32 - 3.0*alpha_max * dr_32);
 #else
         const PS::F64 r_out = EPISoft::r_out;
         const PS::F64 r_out2 = r_out * r_out;
@@ -1558,11 +1563,10 @@ private:
         const PS::F64 drinv2_max = drinv_max*drinv_max;
         const PS::F64 gmor3_max = gmor_max * drinv2_max;
         const PS::F64 alpha_max = drda * drinv2_max;
-
+        const PS::F64vec acorr_max = gmor3_max * (da - 3.0*alpha_max * dr);
 #endif
 
         const PS::F64vec acorr_k = gmor3 * (k*da - (3.0*k*alpha - kdot) * dr);
-        const PS::F64vec acorr_max = gmor3_max * (da - 3.0*alpha_max * dr);
 
         _pi.acorr -= 2.0 * (acorr_k - acorr_max);
         //acci + dt_kick * dt_kick * acorri /48; 
