4159d39da6812463092e3d0014c139ce9f41865d
Long Wang <longwang.astro@live.com>
2022-07-10 16:30:08 +0800

    Fix inconsistence of petar.data.clear with new version

--- tools/data_clear.sh
+++ tools/data_clear.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-suffixes=(esc group sse mosse sseEmp bse mobse bseEmp status prof.rank)
-tindices=(1 3 21 21 21 13 13 13 1 2)
-nsuffixes=8
 # sn single 12 binary 14
+tindex_sse_type_change=21
+tindex_sse_sn_kick=12
+tindex_bse_type_change=23
+tindex_bse_dyn_merge=38
+tindex_bse_sn_kick=14
+
+ncol_sse_type_change=22
+ncol_sse_sn_kick=13
+ncol_bse_type_change=46
+ncol_bse_dyn_merge=49
+ncol_bse_sn_kick=15
+
 unset tcrit
-less_out=0
 
 until [[ `echo x$1` == 'x' ]]
 do
@@ -17,16 +25,22 @@ do
 	    echo 'Options:';
 	    echo '  -t: time criterion (default: none)'
 	    echo '  -n: MPI processes number (default: auto detect)';
-	    echo '  -l: less output mode (three columns less, for the PeTar version before Sep 10, 2020)'
 	    echo '  -b: use previous backup files instead of replacing (default: replacing)';
+	    echo '  --less-dyn-merge: less output mode for [prefix].*bse*.dynmical_merge (three columns less, for the PeTar version before Sep 10, 2020)'
+	    echo '  --less-type-change: less output mode for [prefix].*bse*.type_change (20 columns less, for the PeTar version before Jun 2, 2022)'
 	    exit;;
 	-n) shift; nmpi=$1; shift;;
 	-t) shift; tcrit=$1; shift;;
-	-l) less_out=1;shift;; 
 	-b) rmi=1; shift;;
+	--less-dyn-merge) tindex_bse_dyn_merge=35; ncol_bse_dyn_merge=46;shift;; 
+	--less-type-change) tindex_bse_type_change=13;ncol_bse_type_change=26;shift;;
 	*) fname=$1;shift;;
     esac
 done
+
+suffixes=(esc group sse mosse sseEmp bse mobse bseEmp status prof.rank)
+tindices=(1 3 0 0 0 0 0 0 1 2)
+nsuffixes=${#suffixes[@]}
 
 if [ ! -e $fname ] | [ -z $fname ] ; then
     echo 'Error, file name not provided' 
@@ -41,7 +55,38 @@ fi
 echo 'data filename prefix: '$fname
 echo 'time criterion: '$tcrit
 
-# esc group
+# check consistence for the number of columns
+ncol=`egrep -m 1 'SN_kick' $fname.*sse*.[0-9]|wc -w`
+if [[ $ncol != $ncol_sse_sn_kick ]] && [[ $ncol != 0 ]]; then
+    echo 'Error! column number not matches for SSE SN kick, should be '$ncol_sse_sn_kick', the file has '$ncol'.'
+    exit
+fi
+ncol=`egrep -v -m 1 'SN_kick' $fname.*sse*.[0-9]|wc -w`
+if [[ $ncol -ne $ncol_sse_type_change ]] && [[ $ncol -ne 0 ]]; then
+    echo 'Error! column number not matches for SSE Type Change, should be '$ncol_sse_type_change', the file has '$ncol
+    exit
+fi
+
+ncol=`egrep -m 1 'Dynamic_merge' $fname.*bse*.[0-9]|wc -w`
+if [[ $ncol -ne $ncol_bse_dyn_merge ]] && [[ $ncol -ne 0 ]]; then
+    echo 'Error! column number not matches for BSE dynamical merger, should be '$ncol_bse_dyn_merge', the file has '$ncol
+    exit
+fi
+
+ncol=`egrep -m 1 'SN_kick' $fname.*bse*.[0-9]|wc -w`
+if [[ $ncol -ne $ncol_bse_sn_kick ]] && [[ $ncol -ne 0 ]]; then
+    echo 'Error! column number not matches for BSE SN kick, should be '$ncol_bse_sn_kick', the file has '$ncol
+    exit
+fi
+
+ncol=`egrep -v -m 1 '(SN_kick|Dynamic_merge)' $fname.*bse*.[0-9]|wc -w`
+if [[ $ncol -ne $ncol_bse_type_change ]] && [[ $ncol -ne 0 ]]; then
+    echo 'Error! column number not matches for BSE Type Change, should be '$ncol_bse_type_change', the file has '$ncol
+    exit
+fi
+
+
+# clear each file
 for ((i=0;i<$nsuffixes;i=i+1))
 do
     s=${suffixes[$i]}
@@ -63,18 +108,16 @@ do
 	    if [ -z $rmi ]; then
 		rm -i $f.bk
 		mv $f $f.bk
+		echo 'backup '$f' to '$f.bk
 	    fi
 	else
 	    mv $f $f.bk
+	    echo 'backup '$f' to '$f.bk
 	fi
 	if [[ $s == *'sse'* ]]; then
-	    awk -v t=$tcrit -v ti=$tindex '{if (($1!="SN_kick" && $ti<=t) || ($1=="SN_kick" && $12<=t)) print $LINE}' $f.bk >$f
+	    awk -v t=$tcrit -v tsn=$tindex_sse_sn_kick -v ttch=$tindex_sse_type_change '{if (($1!="SN_kick" && $ttch<=t) || ($1=="SN_kick" && $tsn<=t)) print $LINE}' $f.bk >$f
 	elif [[ $s == *'bse'* ]]; then
-	    if [ $less_out == 0 ]; then
-		awk -v t=$tcrit -v ti=$tindex '{if ($1=="SN_kick") {if ($14<=t) print $LINE} else if ($1=="Dynamic_merge:") {if($38<=t) print $LINE} else if ($ti<=t) print $LINE;}' $f.bk >$f
-	    else
-		awk -v t=$tcrit -v ti=$tindex '{if ($1=="SN_kick") {if ($14<=t) print $LINE} else if ($1=="Dynamic_merge:") {if($35<=t) print $LINE} else if ($ti<=t) print $LINE;}' $f.bk >$f
-	    fi
+	    awk -v t=$tcrit -v tsn=$tindex_bse_sn_kick -v ttch=$tindex_bse_type_change -v tdyn=$tindex_bse_dyn_merge '{if ($1=="SN_kick") {if ($tsn<=t) print $LINE} else if ($1=="Dynamic_merge:") {if($tdyn<=t) print $LINE} else if ($ttch<=t) print $LINE;}' $f.bk >$f
 	else
 	    awk -v t=$tcrit -v ti=$tindex '{if ($ti<=t) print $LINE}' $f.bk >$f
 	fi
