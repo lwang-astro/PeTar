4645f103e8c3ef6cd43aa48d0bad7abf4e308b0e
Long Wang <longwang.astro@live.com>
2025-11-26 13:09:08 +0800

    suppress the use of KDTree for evaluating potential since it is not consistent with changeover function

--- src/hermite_interaction.hpp
+++ src/hermite_interaction.hpp
@@ -509,17 +509,17 @@ public:
     template<class Tp, class Tgroup, class Tpert>
     inline void calcEnergy(H4::HermiteEnergy& _energy, const Tp* _particles, const int _n_particle, const Tgroup* _groups, const int* _group_index, const int _n_group, const Tpert& _perturber) {
         _energy.ekin = _energy.epot = _energy.epert = 0.0;
-#ifdef HERMITE_ONLY_CALC_NEIGHBOR_FORCE
-        // in this mode, we only calculate the energy of neighbor particles
+#ifdef HERMITE_ONLY_CALC_NEIGHBOR_FORCE_POT
+        // in this mode, we only calculate the energy of neighbor particles, should not be used, this is incorrect for changeover potential.
         COMM::ParticleKDTree kdtree;
-        kdtree.addParticles(_particles, _n_particle);
+        kdtree.addParticles(_particles, nullptr, _n_particle);
 #endif
         for (int i=0; i<_n_particle; i++) {
             auto& pi = _particles[i];
             if (pi.mass==0.0) continue;
             _energy.ekin += pi.mass* (pi.vel[0]*pi.vel[0] + pi.vel[1]*pi.vel[1] + pi.vel[2]*pi.vel[2]);
             Float poti = 0.0;
-#ifdef HERMITE_ONLY_CALC_NEIGHBOR_FORCE            
+#ifdef HERMITE_ONLY_CALC_NEIGHBOR_FORCE_POT        
             kdtree.searchNeighborParticlesApply(pi, [&](int j_idx) {
                 const auto& pj = _particles[j_idx];
                 if (pi.id == pj.id) return;
@@ -541,7 +541,7 @@ public:
                 const Float k = ChangeOver::calcPotWTwo(pi.changeover, pj.changeover, r);
         
                 poti += -pj.mass*rinv*k;
-#ifdef HERMITE_ONLY_CALC_NEIGHBOR_FORCE                
+#ifdef HERMITE_ONLY_CALC_NEIGHBOR_FORCE_POT            
             });
 #else
             }
@@ -566,7 +566,7 @@ public:
         }
 #endif
         _energy.ekin *= 0.5;
-#ifdef HERMITE_ONLY_CALC_NEIGHBOR_FORCE
+#ifdef HERMITE_ONLY_CALC_NEIGHBOR_FORCE_POT
         _energy.epot *= 0.5;
 #endif
         //_energy.epert *= 0.5;
